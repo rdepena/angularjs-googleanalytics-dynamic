cd12840dd8eb0d4804cb8c9fa5aeffbf9b3c5ac9
Ricardo De Pena <ricardo.depena@gmail.com>
2013-06-18 14:43:18 -0400

    changes to fit a specific scenario where multiple apps can be configured dynamically

--- analyticsService.js
+++ analyticsService.js
@@ -1,25 +1,29 @@
 var _gaq = _gaq || [];
 
-angular.module('analytics', []).run(['$http', function($http) {
+angular.module('analytics', []).run(['$http', function ($http) {
 
-	_gaq.push(['_setAccount', 'YOUR GOOGLE ACCOUNT']);
-	_gaq.push(['_trackPageview']);
+}]).service('analytics', function ($rootScope, $window, $location, $routeParams) {
 
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0];
-	s.parentNode.insertBefore(ga, s);
+	var init = function (accountCodes) {
+		angular.forEach(accountCodes, function (accountCode) {
+			_gaq.push(['_setAccount', accountCode]);
+			_gaq.push(['_trackPageview']);
+		});
 
-}]).service('analytics', function($rootScope, $window, $location, $routeParams) {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0];
+		s.parentNode.insertBefore(ga, s);
 
-	$rootScope.$on('$viewContentLoaded', track);
-
+		$rootScope.$on('$viewContentLoaded', track);
+	}
+	
 	var track = function() {
 		var path = convertPathToQueryString($location.path(), $routeParams)
 		$window._gaq.push(['_trackPageview', path]);
 	};
 	
-	var convertPathToQueryString = function(path, $routeParams) {
+	var convertPathToQueryString = function (path, $routeParams) {
 		for (var key in $routeParams) {
 			var queryParam = '/' + $routeParams[key];
 			path = path.replace(queryParam, '');
